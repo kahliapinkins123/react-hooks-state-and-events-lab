05d2618d07665375a114b1bac344f76363610db0
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-11-29 16:39:26 -0500

    commit tree c4238db0deb4f1e95d19df850992c2c3d4e38026

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(true)
+
+  let inCartClass = inCart ? "":"in-cart"
+
+  function cartClick(e){
+    if(e.target.innerText === "Add to Cart"){
+      e.target.innerText = "Remove from Cart"
+      setInCart(!inCart)
+    } else{
+      e.target.innerText = "Add to Cart"
+      setInCart(!inCart)
+    }
+  }
+
   return (
-    <li className="">
+    <li className={inCartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={cartClick}>Add to Cart</button>
     </li>
   );
 }
